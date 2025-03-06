b954f7cfd5276ce94efe083c6a37df4c2becefb5
Alejandro <alejandrolujan1999@outlook.es>
2025-03-06 23:58:35 +0000

    version 1.1

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,11 +1,25 @@
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Check the <a target="_blank" href="https://4geeks.com/docs/start/react-flask-template">template documentation</a> <i className="fa-solid fa-file"></i> for help.
-		</p>
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+  <footer className="footer bg-dark py-3 text-center text-light mt-auto shadow-sm">
+    <div className="container">
+      <p>
+        Check the{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://4geeks.com/docs/start/react-flask-template"
+          className="text-light"
+        >
+          template documentation
+        </a>{" "}
+        <i className="fa-solid fa-file"></i> for help.
+      </p>
+      <p>
+        Made with{" "}
+        <i className="fa fa-heart text-danger" /> by{" "}
+        <a href="http://www.4geeksacademy.com" className="text-light">
+          4Geeks Academy
+        </a>
+      </p>
+    </div>
+  </footer>
 );
--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,19 +1,38 @@
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">
+          Home
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link to="/Login" className="nav-link">
+                <button className="btn btn-outline-primary">Login</button>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/Signup" className="nav-link">
+                <button className="btn btn-outline-primary">Signup</button>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
